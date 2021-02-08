402ccf5f1354cd94a7e093d28937146e3205599a
Atanas Tankov <atanas.tankov@accedia.com>
2021-02-08 18:15:07 +0200

    Add routing to the project and Orders page which fetch the data from Firebase source

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,28 +92,16 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('Continue');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Atanas Todorov',
-                address: {
-                    street: 'Test street',
-                    zipCode: '1111',
-                    country: 'Bulgaria'
-                },
-                email: 'test@abv.bg'
-            },
-            deliveryMethod: 'free'
-        };
-
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing: false});
-        })
-        .catch(error => {
-            this.setState({loading: false, purchasing: false}); 
+       
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
         });
     }
 
